ee01a815c22c97953bb60507028a3dc9f8850a46
Raphaël Duhen <raphael.duhen.nexus@gmail.com>
2026-02-26 02:32:35 +0100

    feat: fill in gui anchor positions

--- reframework/autorun/mdk/game/gui/Anchor.lua
+++ reframework/autorun/mdk/game/gui/Anchor.lua
@@ -1,6 +1,15 @@
 ---@enum Anchor
+---Corresponds to [via.gui.ControlPoint].
 local Anchor = {
-  bottom_left = 10,
+  top_left = 0,
+  top_center = 1,
+  top_right = 2,
+  center_left = 4,
+  center_center = 5,
+  center_right = 6,
+  bottom_left = 8,
+  bottom_center = 9,
+  bottom_right = 10,
 }
 
 return Anchor
